52fdcb60083745d4396b2388b86d31284caab776
Bruno Szczuk <brunoszczuk@gmail.com>
2022-04-02 18:08:56 -0300

    created a unsuccessfull scenario

--- src/test/kotlin/br/com/hellopizza/api/core/usecase/impl/CreateSizeUseCaseImplTest.kt
+++ src/test/kotlin/br/com/hellopizza/api/core/usecase/impl/CreateSizeUseCaseImplTest.kt
@@ -13,11 +13,11 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.Mockito.`when`
+import org.mockito.Mockito.never
 import java.util.*
 
 internal class CreateSizeUseCaseImplTest {
@@ -45,6 +45,28 @@ internal class CreateSizeUseCaseImplTest {
         assertTrue(result.violations.isEmpty())
         assertEquals(result.sizeSumarryDTO, size.summaryDTO())
         verify(sizeGateway).save(preparedSize)
+    }
+
+    @ExperimentalCoroutinesApi
+    @ParameterizedTest(name = "Create Size {0} Unsuccessfully.")
+    @MethodSource("br.com.hellopizza.api.core.domain.pizza.SizeFactoriesKt#defaultSizes")
+    fun `should not create a Size because the validation is failed`(size: Size) = runTest {
+        //Given
+        val preparedSize = size.prepareForCreate()
+        val savedSize = Optional.of(size)
+        `when`(sizeGateway.findByDescriptionAndToppingLimitAndDefaultPrice(size.description, size.toppingLimit, size.defaultPrice))
+                .thenReturn(savedSize)
+        `when`(executor.validate(rules, Optional.of(preparedSize), savedSize))
+                .thenReturn(ValidationResult.error("error"))
+        `when`(sizeGateway.save(preparedSize)).thenReturn(size)
+
+        //When
+        val result = createSizeUseCaseImpl.execute(size.createSizeCommand())
+
+        //Then
+        assertFalse(result.violations.isEmpty())
+        assertEquals(result.sizeSumarryDTO, size.summaryDTO())
+        verify(sizeGateway, never()).save(preparedSize)
     }
 
 }
